e9dbdbdae99be9c9ea3dd99e453b102d9c1059e2
dCOMMICS <sanwurld@gmail.com>
2023-09-04 12:21:35 +0300

    memorize leetcode

---
+++ 30 days of JS/2623.ts
@@ -0,0 +1,32 @@
+type Fn = (...params: any) => any
+
+function memoize(fn: Fn): Fn {
+
+    let map = new Map();
+    let ans = function(...params:any):any
+    {
+        let input:string = params.join(",").toString();
+        if(map.get(input) == undefined)
+        {
+            map.set(input,fn(...params));
+        }
+        return map.get(input);
+        
+    }
+
+    
+    return ans;
+    
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
