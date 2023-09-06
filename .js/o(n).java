38be6365d976fcb3ac7f1112bd3145e00339aad2
dCOMMICS <sanwurld@gmail.com>
2023-09-06 14:59:47 +0300

    repeated substring pattern

--- .js/o(n).java
+++ .js/o(n).java
@@ -49,4 +49,20 @@ class Solution {
         }
         return res;
     }
+
+}
+
+// repeated substring pattern//
+
+public class Solution {
+    public boolean repeatedSubstringPattern(String str) {
+        for (int i = 1; i <= str.length() / 2; i ++) {
+            if (str.length() % i == 0) {
+                if (str.substring(0, str.length() - i).equals(str.substring(i))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
