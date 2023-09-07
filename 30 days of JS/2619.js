5a35f2749e42daeace38172082228608f6a7eb51
dCOMMICS <sanwurld@gmail.com>
2023-09-07 15:45:02 +0300

    runtime 52.3ms",

---
+++ 30 days of JS/2619.js
@@ -0,0 +1,13 @@
+Array.prototype.last = function() {
+    
+    if(this.length === 0){
+        console.log(-1)
+        return -1
+    }
+    
+    console.log(this[this.length -1])
+    return this[this.length -1]
+};
+
+
+// array  prototype last //
