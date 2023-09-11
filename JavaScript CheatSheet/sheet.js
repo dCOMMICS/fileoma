47ff62cd85f3035d83ad820d05d3581c2b75e73a
dCOMMICS <sanwurld@gmail.com>
2023-09-11 20:05:53 +0300

    for loop

---
+++ JavaScript CheatSheet/sheet.js
@@ -0,0 +1,19 @@
+// loops //
+// 1 for loop //
+
+for (var h = 0; h < 10; h++){
+    document.write (h + ":" + h *3 + "<br/>");
+
+    var sum = 0;
+    for (var h = 0; h < a.length; h++ ) {
+        sum += a[h]; // parsing an array 
+    }
+    html = "";
+
+    for (var h of custOrder){
+        html += "<li>" + h +"</li>";
+    }
+}
+
+
+// while loop 
